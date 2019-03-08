e96349fdc6ed41f18a6b542932d118e90b74a60e
Pablo Rodriguez <pablo.rodriguez-quesada@windriver.com>
2019-03-07 23:19:48 -0600

    get set image

--- src/image.c
+++ src/image.c
@@ -15,9 +15,9 @@ void free_image_png(PNGImage image){
 }
 
 Pixel get_pixel(Image image,int x, int y){
-    return &(image.Rows[y])[x];
+    return image.Rows[y][x];
 }
 
 void set_pixel(Image image, int x, int y, Pixel val){
-    &(image.Rows[y])[x] = val;
+    image.Rows[y][x] = val;
 }
