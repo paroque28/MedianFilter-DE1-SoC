148d256ee8a1b6a0eb3f747e2aa7e50156735a72
Pablo Rodriguez <pablo.rodriguez-quesada@windriver.com>
2019-03-07 23:16:57 -0600

    get set image

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ DEPS= src/image.c src/utils/split.c src/utils/convert.c src/utils/print.c src/ut
 HEADERS= src/image.h src/utils/split.h src/utils/convert.h src/utils/print.h src/utils/io.h
 
 split: src/main.c $(DEPS) $(HEADERS)
-	$(CC) -o $@ src/main.c  $(DEPS) $(CFLAGS) -g -ggdb
+	$(CC) -o $@ src/main.1.c  $(DEPS) $(CFLAGS) -g -ggdb
--- src/image.c
+++ src/image.c
@@ -14,9 +14,10 @@ void free_image_png(PNGImage image){
     free(image.Rows);
 }
 
-unsigned char get_pixel(Image image,int x, int y){
-
+Pixel get_pixel(Image image,int x, int y){
+    return &(image.Rows[y])[x];
 }
-void set_pixel(Image image, int x, int y, unsigned char val){
-    
+
+void set_pixel(Image image, int x, int y, Pixel val){
+    &(image.Rows[y])[x] = val;
 }
