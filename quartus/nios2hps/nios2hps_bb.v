4dcf7a56e767ad93b951610b6ed1eedd3c23e84b
Pablo Rodriguez <pablo.rodriguez-quesada@windriver.com>
2019-03-16 18:07:24 -0600

    HPS unstable

--- quartus/nios2hps/nios2hps_bb.v
+++ quartus/nios2hps/nios2hps_bb.v
@@ -1,8 +1,9 @@
 
 module nios2hps (
 	clk_clk,
-	reset_reset_n,
+	leds_export,
 	pll_off_sdram_clk,
+	reset_reset_n,
 	sdram_controller_wire_addr,
 	sdram_controller_wire_ba,
 	sdram_controller_wire_cas_n,
@@ -12,11 +13,71 @@ module nios2hps (
 	sdram_controller_wire_dqm,
 	sdram_controller_wire_ras_n,
 	sdram_controller_wire_we_n,
-	leds_export);	
+	hps_ddr_mem_a,
+	hps_ddr_mem_ba,
+	hps_ddr_mem_ck,
+	hps_ddr_mem_ck_n,
+	hps_ddr_mem_cke,
+	hps_ddr_mem_cs_n,
+	hps_ddr_mem_ras_n,
+	hps_ddr_mem_cas_n,
+	hps_ddr_mem_we_n,
+	hps_ddr_mem_reset_n,
+	hps_ddr_mem_dq,
+	hps_ddr_mem_dqs,
+	hps_ddr_mem_dqs_n,
+	hps_ddr_mem_odt,
+	hps_ddr_mem_dm,
+	hps_ddr_oct_rzqin,
+	hps_io_hps_io_emac1_inst_TX_CLK,
+	hps_io_hps_io_emac1_inst_TXD0,
+	hps_io_hps_io_emac1_inst_TXD1,
+	hps_io_hps_io_emac1_inst_TXD2,
+	hps_io_hps_io_emac1_inst_TXD3,
+	hps_io_hps_io_emac1_inst_RXD0,
+	hps_io_hps_io_emac1_inst_MDIO,
+	hps_io_hps_io_emac1_inst_MDC,
+	hps_io_hps_io_emac1_inst_RX_CTL,
+	hps_io_hps_io_emac1_inst_TX_CTL,
+	hps_io_hps_io_emac1_inst_RX_CLK,
+	hps_io_hps_io_emac1_inst_RXD1,
+	hps_io_hps_io_emac1_inst_RXD2,
+	hps_io_hps_io_emac1_inst_RXD3,
+	hps_io_hps_io_qspi_inst_IO0,
+	hps_io_hps_io_qspi_inst_IO1,
+	hps_io_hps_io_qspi_inst_IO2,
+	hps_io_hps_io_qspi_inst_IO3,
+	hps_io_hps_io_qspi_inst_SS0,
+	hps_io_hps_io_qspi_inst_CLK,
+	hps_io_hps_io_sdio_inst_CMD,
+	hps_io_hps_io_sdio_inst_D0,
+	hps_io_hps_io_sdio_inst_D1,
+	hps_io_hps_io_sdio_inst_CLK,
+	hps_io_hps_io_sdio_inst_D2,
+	hps_io_hps_io_sdio_inst_D3,
+	hps_io_hps_io_usb1_inst_D0,
+	hps_io_hps_io_usb1_inst_D1,
+	hps_io_hps_io_usb1_inst_D2,
+	hps_io_hps_io_usb1_inst_D3,
+	hps_io_hps_io_usb1_inst_D4,
+	hps_io_hps_io_usb1_inst_D5,
+	hps_io_hps_io_usb1_inst_D6,
+	hps_io_hps_io_usb1_inst_D7,
+	hps_io_hps_io_usb1_inst_CLK,
+	hps_io_hps_io_usb1_inst_STP,
+	hps_io_hps_io_usb1_inst_DIR,
+	hps_io_hps_io_usb1_inst_NXT,
+	hps_io_hps_io_spim1_inst_CLK,
+	hps_io_hps_io_spim1_inst_MOSI,
+	hps_io_hps_io_spim1_inst_MISO,
+	hps_io_hps_io_spim1_inst_SS0,
+	hps_io_hps_io_uart0_inst_RX,
+	hps_io_hps_io_uart0_inst_TX);	
 
 	input		clk_clk;
-	input		reset_reset_n;
+	output	[7:0]	leds_export;
 	output		pll_off_sdram_clk;
+	input		reset_reset_n;
 	output	[12:0]	sdram_controller_wire_addr;
 	output	[1:0]	sdram_controller_wire_ba;
 	output		sdram_controller_wire_cas_n;
@@ -26,5 +87,64 @@ module nios2hps (
 	output	[1:0]	sdram_controller_wire_dqm;
 	output		sdram_controller_wire_ras_n;
 	output		sdram_controller_wire_we_n;
-	output	[7:0]	leds_export;
+	output	[14:0]	hps_ddr_mem_a;
+	output	[2:0]	hps_ddr_mem_ba;
+	output		hps_ddr_mem_ck;
+	output		hps_ddr_mem_ck_n;
+	output		hps_ddr_mem_cke;
+	output		hps_ddr_mem_cs_n;
+	output		hps_ddr_mem_ras_n;
+	output		hps_ddr_mem_cas_n;
+	output		hps_ddr_mem_we_n;
+	output		hps_ddr_mem_reset_n;
+	inout	[31:0]	hps_ddr_mem_dq;
+	inout	[3:0]	hps_ddr_mem_dqs;
+	inout	[3:0]	hps_ddr_mem_dqs_n;
+	output		hps_ddr_mem_odt;
+	output	[3:0]	hps_ddr_mem_dm;
+	input		hps_ddr_oct_rzqin;
+	output		hps_io_hps_io_emac1_inst_TX_CLK;
+	output		hps_io_hps_io_emac1_inst_TXD0;
+	output		hps_io_hps_io_emac1_inst_TXD1;
+	output		hps_io_hps_io_emac1_inst_TXD2;
+	output		hps_io_hps_io_emac1_inst_TXD3;
+	input		hps_io_hps_io_emac1_inst_RXD0;
+	inout		hps_io_hps_io_emac1_inst_MDIO;
+	output		hps_io_hps_io_emac1_inst_MDC;
+	input		hps_io_hps_io_emac1_inst_RX_CTL;
+	output		hps_io_hps_io_emac1_inst_TX_CTL;
+	input		hps_io_hps_io_emac1_inst_RX_CLK;
+	input		hps_io_hps_io_emac1_inst_RXD1;
+	input		hps_io_hps_io_emac1_inst_RXD2;
+	input		hps_io_hps_io_emac1_inst_RXD3;
+	inout		hps_io_hps_io_qspi_inst_IO0;
+	inout		hps_io_hps_io_qspi_inst_IO1;
+	inout		hps_io_hps_io_qspi_inst_IO2;
+	inout		hps_io_hps_io_qspi_inst_IO3;
+	output		hps_io_hps_io_qspi_inst_SS0;
+	output		hps_io_hps_io_qspi_inst_CLK;
+	inout		hps_io_hps_io_sdio_inst_CMD;
+	inout		hps_io_hps_io_sdio_inst_D0;
+	inout		hps_io_hps_io_sdio_inst_D1;
+	output		hps_io_hps_io_sdio_inst_CLK;
+	inout		hps_io_hps_io_sdio_inst_D2;
+	inout		hps_io_hps_io_sdio_inst_D3;
+	inout		hps_io_hps_io_usb1_inst_D0;
+	inout		hps_io_hps_io_usb1_inst_D1;
+	inout		hps_io_hps_io_usb1_inst_D2;
+	inout		hps_io_hps_io_usb1_inst_D3;
+	inout		hps_io_hps_io_usb1_inst_D4;
+	inout		hps_io_hps_io_usb1_inst_D5;
+	inout		hps_io_hps_io_usb1_inst_D6;
+	inout		hps_io_hps_io_usb1_inst_D7;
+	input		hps_io_hps_io_usb1_inst_CLK;
+	output		hps_io_hps_io_usb1_inst_STP;
+	input		hps_io_hps_io_usb1_inst_DIR;
+	input		hps_io_hps_io_usb1_inst_NXT;
+	output		hps_io_hps_io_spim1_inst_CLK;
+	output		hps_io_hps_io_spim1_inst_MOSI;
+	input		hps_io_hps_io_spim1_inst_MISO;
+	output		hps_io_hps_io_spim1_inst_SS0;
+	input		hps_io_hps_io_uart0_inst_RX;
+	output		hps_io_hps_io_uart0_inst_TX;
 endmodule
