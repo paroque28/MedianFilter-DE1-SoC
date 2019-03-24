09bc67a876e545c0d64da209f6a9f1359da06f69
Pablo Rodriguez <paroque28@gmail.com>
2019-03-24 15:46:21 -0600

    Update ARM.c

--- ARM/src/ARM.c
+++ ARM/src/ARM.c
@@ -26,25 +26,6 @@ int main(int argc, char *argv[])
 	int fd;
 	int i;
 
-
-	// Get Image properties
-	png_props input_image_props = get_png_props(argv[1]);
-	ImgSize input_image_size = {0,0,input_image_props.width, input_image_props.height};
-	// Get Matrix from image
-	PNGImage png_image = {read_png_file(argv[1]), input_image_size};
-
-
-
-
-	Image image_matrix = png_to_Image(png_image);
-
-	Image img2 = medianFilter5x5(image_matrix);
-	png_image = Image_to_png(img2);
-	write_png_file("5x5.png", png_image);
-
-
-
-
 	fd = open("/dev/mem", (O_RDWR | O_SYNC));
 
 
