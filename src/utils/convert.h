02ee039cf32705eb1809ebf26d6893e74e8de7bf
Pablo Rodriguez <pablo.rodriguez-quesada@windriver.com>
2019-03-24 18:27:33 -0600

    pmg initial

--- src/utils/convert.h
+++ src/utils/convert.h
@@ -2,13 +2,14 @@
 #define CONVERT_H
 
 #include "../image.h"
-#include "../imagepng.h"
+// #include "../imagepng.h"
+#include "pmg.h"
 #include "../imagenios.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-Image png_to_Image (PNGImage);    
-PNGImage Image_to_png (Image);   
+// Image png_to_Image (PNGImage);    
+// PNGImage Image_to_png (Image);   
 
 ImageNIOS convertImageToImageNIOS (Image);
 Image convertNIOSImageToImage (ImageNIOS);
