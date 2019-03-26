a18f04c89607acb9a593b4c1222b7f6bc6e03ae4
Pablo Rodriguez <pablo.rodriguez-quesada@windriver.com>
2019-03-26 15:41:11 -0600

    WORKING!!!! NIOS 2 filter

--- nios2/main.c
+++ nios2/main.c
@@ -15,13 +15,22 @@ int main()
 
   printf("SDRAM Memory offset: %X\n", sdram);
   printf("SDRAM SPAN: %X\n", SDRAM_CONTROLLER_BASE);
+//  while(1){
 
-  while (*((int*)leds) != IMAGE_SENT_TO_NIOS);
-  ImageNIOS fromARM = receiveFromSDRAM(sdram,leds,IMAGE_RECEIVED_ON_NIOS);
-  Image fromARMconv = convertNIOSImageToImage(fromARM);
-  Image filtered_img_arm = medianFilter5x5(fromARMconv);
-  ImageNIOS toARM = convertImageToImageNIOS(filtered_img_arm);
-  sendToSDRAM (sdram, leds, IMAGE_SENT_TO_ARM, toARM);
-
+	  printf("NIOS waiting for input image\n");
+	  while (*((int*)leds) != IMAGE_SENT_TO_NIOS){
+		  //printf("%X\n",*((int*)leds));
+		  usleep(100000);
+	  }
+	  printf("NIOS receiving input image\n");
+	  ImageNIOS fromARM = receiveFromSDRAM(sdram,leds,IMAGE_RECEIVED_ON_NIOS);
+	  Image fromARMconv = convertNIOSImageToImage(fromARM);
+	  printf("NIOS filtering input image\n");
+	  Image filtered_img_arm = medianFilter5x5(fromARMconv);
+	  printf("NIOS filtered input image\n");
+	  ImageNIOS toARM = convertImageToImageNIOS(filtered_img_arm);
+	  sendToSDRAM (sdram, leds, IMAGE_SENT_TO_ARM, toARM);
+	  printf("NIOS sent output image\n");
+//  }
   return 0;
 }
--- nios2/utils/convert.h
+++ nios2/utils/convert.h
@@ -2,20 +2,9 @@
 #define CONVERT_H
 
 #include "../image.h"
-<<<<<<< HEAD:nios2/utils/convert.h
-=======
-// #include "../imagepng.h"
-#include "pgm.h"
->>>>>>> e1587680db274f50bac8c66c887053779dc501fd:src/utils/convert.h
 #include "../imagenios.h"
 #include <stdlib.h>
 #include <stdio.h>
-
-<<<<<<< HEAD:nios2/utils/convert.h
-=======
-// Image png_to_Image (PNGImage);    
-// PNGImage Image_to_png (Image);   
->>>>>>> e1587680db274f50bac8c66c887053779dc501fd:src/utils/convert.h
 
 ImageNIOS convertImageToImageNIOS (Image);
 Image convertNIOSImageToImage (ImageNIOS);
